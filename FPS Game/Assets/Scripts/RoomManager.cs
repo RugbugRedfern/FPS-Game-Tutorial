1bcf79763ba70dca50abc319236f2e97b74ad699
Red <rugbugredfern@gmail.com>
2020-07-10 23:04:19 -0700

    Ep 5

---
+++ FPS Game/Assets/Scripts/RoomManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+	public static RoomManager Instance;
+
+	void Awake()
+	{
+		if(Instance)
+		{
+			Destroy(gameObject);
+			return;
+		}
+		DontDestroyOnLoad(gameObject);
+		Instance = this;
+	}
+
+	public override void OnEnable()
+	{
+		base.OnEnable();
+		SceneManager.sceneLoaded += OnSceneLoaded;
+	}
+
+	public override void OnDisable()
+	{
+		base.OnDisable();
+		SceneManager.sceneLoaded -= OnSceneLoaded;
+	}
+
+	void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
+	{
+		if(scene.buildIndex == 1) // We're in the game scene
+		{
+			PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerManager"), Vector3.zero, Quaternion.identity);
+		}
+	}
+}
