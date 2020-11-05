a1667e50b72f75ade0b208e2f622c6b39c075d79
Red <rugbugredfern@gmail.com>
2020-11-04 22:59:45 -0800

    9

--- FPS Game/Assets/Scripts/PlayerController.cs
+++ FPS Game/Assets/Scripts/PlayerController.cs
@@ -1,9 +1,11 @@
 ﻿using Photon.Pun;
+using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviourPunCallbacks
 {
 	[SerializeField] GameObject cameraHolder;
 
@@ -124,6 +126,21 @@ public class PlayerController : MonoBehaviour
 		}
 
 		previousItemIndex = itemIndex;
+
+		if(PV.IsMine)
+		{
+			Hashtable hash = new Hashtable();
+			hash.Add("itemIndex", itemIndex);
+			PhotonNetwork.LocalPlayer.SetCustomProperties(hash);
+		}
+	}
+
+	public override void OnPlayerPropertiesUpdate(Player targetPlayer, Hashtable changedProps)
+	{
+		if(!PV.IsMine && targetPlayer == PV.Owner)
+		{
+			EquipItem((int)changedProps["itemIndex"]);
+		}
 	}
 
 	public void SetGroundedState(bool _grounded)
