894ade16606e07893a03cd4f1b43e82f075231b1
Red <rugbugredfern@gmail.com>
2020-12-22 14:57:01 -0800

    E12

---
+++ FPS Game/Assets/SpawnManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+	public static SpawnManager Instance;
+
+	Spawnpoint[] spawnpoints;
+
+	void Awake()
+	{
+		Instance = this;
+		spawnpoints = GetComponentsInChildren<Spawnpoint>();
+	}
+
+	public Transform GetSpawnpoint()
+	{
+		return spawnpoints[Random.Range(0, spawnpoints.Length)].transform;
+	}
+}
